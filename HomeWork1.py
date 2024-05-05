2a3879ce9f8a91773efc81998e5e41847006c8ea
GluttonyGula <168459225+GluttonyGula@users.noreply.github.com>
2024-05-05 14:37:48 +0400

    Add files via upload

---
+++ HomeWork1.py
@@ -0,0 +1,35 @@
+decision = (int(input("5+5*2= ")))
+if decision > 15 or decision < 15:
+    print('Не расстраивайся, попытайся снова')
+if decision == 15:
+    print('Молодец!')
+
+x = 38
+print('дратути!')
+if x < 0:
+    print('Меньше нуля')
+print('дотвидания!')
+
+a, b = 10, 5
+if a > b:
+    print('a > b')
+if a > b and a > 0:
+    print('успех')
+if (a > b) and (a > 0 or b < 1000):
+    print('успех')
+if 5 < b and b < 10:
+    print('успех')
+
+if '34' > '123':
+    print('успех')
+if '123' > '12':
+    print('успех')
+if [1, 2] > [1, 1]:
+    print('успех')
+
+if '6' > 5:
+    print('успех')
+if [5< 6] > 5:
+    print('успех')
+if '6' != 5:
+    print('успех')
